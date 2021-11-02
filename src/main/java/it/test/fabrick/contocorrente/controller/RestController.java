f8e685447090eaaa89bb6401004e4e6c7b732adc
fffogacci <francescafogacci1@gmail.com>
2021-11-02 23:45:49 +0100

    refactoring: estratto costanti; fattorizzato header

--- src/main/java/it/test/fabrick/contocorrente/controller/RestController.java
+++ src/main/java/it/test/fabrick/contocorrente/controller/RestController.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,17 +17,30 @@ import java.util.Map;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    final String ACCOUNT_ID = "14537780";
+
+    private final String ACCOUNT_ID = "14537780";
+    private final String DOMAIN = "https://sandbox.platfr.io";
+    private final String APIKEY = "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP";
+    private final String S2S = "S2S";
+
+    private HttpHeaders getHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Api-key", APIKEY);
+        headers.set("Auth-Schema", S2S);
+        return headers;
+    }
+
     @GetMapping("/getLetturaSaldo")
     public String letturaSaldo() {
-        String uri = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/" + ACCOUNT_ID + "/balance";
+        String uri = DOMAIN
+                + "/api/gbs/banking/v4.0/accounts/"
+                + ACCOUNT_ID
+                + "/balance";
         RestTemplate restTemplate = new RestTemplate();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Api-key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-        headers.set("Auth-Schema", "S2S");
+        HttpHeaders header = getHeader();
+        HttpEntity entity = new HttpEntity(header);
 
-        HttpEntity entity = new HttpEntity(headers);
         ResponseEntity<String> res = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
         JSONObject jsonObject = new JSONObject(res.getBody());
         JSONObject payload = new JSONObject(jsonObject.get("payload").toString());
@@ -38,11 +49,14 @@ public class RestController {
 
     @GetMapping("/getLetturaTransazioni")
     public String letturaTransazioni() {
-        String uri = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/" + ACCOUNT_ID + "/transactions";
+        String uri = DOMAIN
+                + "/api/gbs/banking/v4.0/accounts/"
+                + ACCOUNT_ID
+                + "/transactions";
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Api-key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-        headers.set("Auth-Schema", "S2S");
+        HttpHeaders header = getHeader();
+        HttpEntity entity = new HttpEntity(header);
+
         String urlTemplate = UriComponentsBuilder.fromHttpUrl(uri)
                 .queryParam("fromAccountingDate", "{fromAccountingDate}")
                 .queryParam("toAccountingDate", "{toAccountingDate}")
@@ -51,7 +65,7 @@ public class RestController {
         Map<String, String> params = new HashMap<>();
         params.put("fromAccountingDate", "2019-01-01");
         params.put("toAccountingDate", "2019-12-01");
-        HttpEntity entity = new HttpEntity(headers);
+
         ResponseEntity<String> res = restTemplate.exchange(urlTemplate, HttpMethod.GET, entity, String.class, params);
         JSONObject jsonObject = new JSONObject(res.getBody());
         return jsonObject.get("payload").toString();
@@ -60,16 +74,17 @@ public class RestController {
 
     @GetMapping("/getBonifico")
     public String bonifico(){
-        String uri = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/" + ACCOUNT_ID + "/payments/money-transfers";
+        String uri = DOMAIN
+                + "/api/gbs/banking/v4.0/accounts/"
+                + ACCOUNT_ID
+                + "/payments/money-transfers";
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Api-key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-        headers.set("Auth-Schema", "S2S");
-        headers.set("X-Time-Zone", "");
+        HttpHeaders header = getHeader();
+        header.set("X-Time-Zone", "");
 
         Bonifico bonifico = new Bonifico(14537780L, "francesca", "bonifico", "euro", "123.5", "2021-11-02");
 
-        HttpEntity<Bonifico> entity = new HttpEntity<Bonifico>(bonifico, headers);
+        HttpEntity<Bonifico> entity = new HttpEntity<Bonifico>(bonifico, header);
         ResponseEntity<String> res = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
 
         return  res.getBody();
