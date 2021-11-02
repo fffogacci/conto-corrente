7c3be9c5d9c2cdb220c473a62c6bdc60ef1ee85d
fffogacci <francescafogacci1@gmail.com>
2021-11-02 13:42:16 +0100

    added method letturaTransazioni

--- src/main/java/it/test/fabrick/contocorrente/controller/RestController.java
+++ src/main/java/it/test/fabrick/contocorrente/controller/RestController.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -23,11 +28,33 @@ public class RestController {
 
         HttpEntity entity = new HttpEntity(headers);
         ResponseEntity<String> res = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-        //String result = restTemplate.getForObject(uri, String.class);
         JSONObject jsonObject = new JSONObject(res.getBody());
         JSONObject payload = new JSONObject(jsonObject.get("payload").toString());
         return payload.get("balance").toString();
 
 
     }
+
+    @GetMapping("/getLetturaTransazioni")
+    public String letturaTransazioni() {
+        String uri = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/14537780/transactions";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Api-key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+        headers.set("Auth-Schema", "S2S");
+        String urlTemplate = UriComponentsBuilder.fromHttpUrl(uri)
+                .queryParam("fromAccountingDate", "{fromAccountingDate}")
+                .queryParam("toAccountingDate", "{toAccountingDate}")
+                .encode()
+                .toUriString();
+        Map<String, String> params = new HashMap<>();
+        params.put("fromAccountingDate", "2019-01-01");
+        params.put("toAccountingDate", "2019-12-01");
+        HttpEntity entity = new HttpEntity(headers);
+        ResponseEntity<String> res = restTemplate.exchange(urlTemplate, HttpMethod.GET, entity, String.class, params);
+        JSONObject jsonObject = new JSONObject(res.getBody());
+        return jsonObject.get("payload").toString();
+
+    }
+
 }
